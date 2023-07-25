01b017549202e9d6f6805cd363e620977ed640ef
iuuuuuuuu <1524514078@qq.com>
2023-07-25 21:35:15 +0800

    添加初始化用户操作

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,4 +1,4 @@
-import { Configuration, App } from '@midwayjs/core';
+import { IMidwayContainer, Configuration, App } from '@midwayjs/core';
 import * as koa from '@midwayjs/koa';
 import * as validate from '@midwayjs/validate';
 import * as info from '@midwayjs/info';
@@ -15,6 +15,7 @@ import * as swagger from '@midwayjs/swagger';
 import * as bull from '@midwayjs/bull';
 import * as staticFile from '@midwayjs/static-file';
 import * as captcha from '@midwayjs/captcha';
+import UserService from './service/user.service';
 
 @Configuration({
   // 启用类名冲突检查
@@ -41,7 +42,9 @@ export class ContainerLifeCycle {
   @App()
   app: koa.Application;
 
-  async onReady() {
+  async onReady(container: IMidwayContainer) {
+    const userService = await container.getAsync(UserService);
+    userService.initUser();
     // add middleware
     this.app.useMiddleware([ReportMiddleware]);
     // add filter
